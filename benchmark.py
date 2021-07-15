433c658858dd12855af8f4dac7234eee877e3b5b
Amaar Quadri <amaarquadri@gmail.com>
2021-07-15 14:36:06 -0400

    added real image

--- benchmark.py
+++ benchmark.py
@@ -5,14 +5,75 @@ import numpy as np
 import cv2
 
 
+def test(filename='rail.png'):
+    start_time = time()
+    # Loads an image
+    src = cv2.imread(cv2.samples.findFile(filename), cv2.IMREAD_GRAYSCALE)
+    # Check if image is loaded fine
+    if src is None:
+        print('Error opening image!')
+        # print('Usage: hough_lines.py [image_name -- default ' + default_file + '] \n')
+        return -1
+
+    dst = cv2.Canny(src, 50, 200, None, 3)
+
+    # Copy edges to the images that will display the results in BGR)
+    cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    cdstP = np.copy(cdst)
+
+    lines = cv2.HoughLines(dst, 1, np.pi / 180, 150, None, 0, 0)
+
+    if lines is not None:
+        for i in range(0, len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+            cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+
+    linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
+
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
+
+    cv2.imshow("Source", src)
+    cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
+    cv2.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
+
+    print((time() - start_time) ** -1, 'Hz')
+    cv2.waitKey()
+
+
 def main():
-    test_image = np.random.random((1920, 1080, 3)).astype(np.uint8)
     start = time()
-    for _ in range(10):
-        work = cv2.GaussianBlur(test_image, (5, 5), 3)
-        work = cv2.Canny(test_image, 3, 3)
+    img = cv2.imread('rail.png')
+
+    edges = cv2.Canny(img, 200, 110)
+    lines = cv2.HoughLinesP(edges, 10, 0.1, 400)
+
+    output = np.copy(img)
+    if lines is not None:
+        for i in range(0, len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+            cv2.line(img, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+
+    cv2.imshow('Edges', output)
+    cv2.waitKey(0)
     print((time() - start) ** -1, 'Hz')
 
 
 if __name__ == '__main__':
-    main()
+    test()
