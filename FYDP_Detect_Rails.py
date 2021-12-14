167537ee0d3bde582ffea40a065f1dda73c8da77
FahmidKader <35272722+FahmidKader@users.noreply.github.com>
2021-12-13 22:28:27 -0800

    p

--- FYDP_Detect_Rails.py
+++ FYDP_Detect_Rails.py
@@ -4,7 +4,7 @@ import numpy as np
 def empty(a):
     pass
 
-path = 'data/OneDrive_2021-10-06/All Photos/DJI_0047.JPG'
+path = 'data/OneDrive_2021-10-06/All Photos/DJI_0042.JPG'
 
 def checklinesdetected(lines):
     if len(lines)==0:
@@ -35,6 +35,40 @@ def parallelchecker(angle1,angle2):
         return 1
     else:
         return 0
+def linedetection(lines,cdst):
+    n2 = 0
+    for n1 in range(0, len(lines)):
+        for rho, theta in lines[n1]:
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+            if n1 == 0:
+                strong_lines[n2] = lines[n1]
+                cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+                print(f'line 1: x1:{x1}, y1:{y1}, x2:{x2}, y2:{y2}')
+                m1, b1 = equationofline(x1, y1, x2, y2)
+                n2 = n2 + 1
+            else:
+                if rho < 0:
+                    rho *= 1
+                    theta -= np.pi
+                closeness_rho = np.isclose(rho, strong_lines[0:n2, 0, 0], atol=10)
+                closeness = np.all([closeness_rho], axis=0)
+                if not any(closeness) and n2 < 2:
+                    strong_lines[n2] = lines[n1]
+                    cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+                    m2, b2 = equationofline(x1, y1, x2, y2)
+                    print(f'line 2: x1:{x1}, y1:{y1}, x2:{x2}, y2:{y2}')
+                    n2 = n2 + 1
+    gauge_measurement(m1,b1,m2,b2)
+
 while True:
     img = cv2.imread(path)
     scale_percent = 20  # percent of original size
@@ -72,63 +106,13 @@ while True:
     minLineLength = 2
     maxLineGap = 10
 
-    n2 = 0
-    if len(lines)==0:
-        cv2.putText(mask,"No lines detected",(0,0),FONT_HERS,1,(0,0,255))
-    for n1 in range(0, len(lines)):
-        for rho, theta in lines[n1]:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
-            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
-            if n1 == 0:
-                strong_lines[n2] = lines[n1]
-                cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
-                print(f'line 1: x1:{x1}, y1:{y1}, x2:{x2}, y2:{y2}')
-                m1, b1 = equationofline(x1, y1, x2, y2)
-                n2 = n2 + 1
-            else:
-                if rho < 0:
-                    rho *= 1
-                    theta -= np.pi
-                closeness_rho = np.isclose(rho, strong_lines[0:n2, 0, 0], atol=10)
-                #closeness_theta = np.isclose(theta, strong_lines[0:n2, 0, 1], atol=np.pi / 10)
-                closeness = np.all([closeness_rho], axis=0)
-                if not any(closeness) and n2 < 2:
-                    strong_lines[n2] = lines[n1]
-                    cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
-                    m2, b2 = equationofline(x1, y1, x2, y2)
-                    print(f'line 2: x1:{x1}, y1:{y1}, x2:{x2}, y2:{y2}')
-                    n2 = n2 + 1
-    gauge_measurement(m1,b1,m2,b2)
-    #if lines is not None:
-    #    for i in range(0, len(lines)):
-     #       rho = lines[i][0][0]
-      #      theta = lines[i][0][1]
-       #     a = np.cos(theta)
-        #    b = np.sin(theta)
-         #   x0 = a * rho
-          #  y0 = b * rho
-           # pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
-            #pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
-            #cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+    if not lines.any():
+        cv2.putText(cdst,"No lines detected",(50,50),0,1,(0,0,255),1,2)
 
-    linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 65, None, 120, 50)
-
-    if linesP is not None:
-        for i in range(0, len(linesP)):
-            l = linesP[i][0]
-            cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
+    else:
+        linedetection(lines,cdst)
 
     cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
-    #cv2.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
-
 
     cv2.waitKey()
     cv2.waitKey(1)
