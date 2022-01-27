5ab2cfca06d441a18f8d151e1427078baa031d00
FahmidKader <35272722+FahmidKader@users.noreply.github.com>
2022-01-26 17:10:14 -0700

    Add files via upload

--- FYDP_Detect_Rails.py
+++ FYDP_Detect_Rails.py
@@ -4,7 +4,7 @@ import numpy as np
 def empty(a):
     pass
 
-path = 'data/OneDrive_2021-10-06/All Photos/DJI_0046.JPG'
+path = 'data/OneDrive_2021-10-06/All Photos/DJI_0047.JPG'
 
 def equationoflineVector(x1,y1,x2,y2):
     m=(y2-y1)/(x2-x1)
@@ -13,6 +13,12 @@ def equationoflineVector(x1,y1,x2,y2):
     #x=t
     #y=(m(t-b))
 
+def ccw(A,B,C):
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+
+# Return true if line segments AB and CD intersect
+def intersect(A,B,C,D):
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
 def gauge_measurement(m1,b1,m2,b2):
     y=0
@@ -64,13 +70,16 @@ def linedetection(lines,cdst):
                 print(f'line {n2+1}: x1:{x1}, y1:{y1}, x2:{x2}, y2:{y2}')
                 #m1, b1 = equationofline(x1, y1, x2, y2)
                 n2 = n2 + 1
+                pt1og=pt1
+                pt2og=pt2
+                theta1=theta
             else:
                 if rho < 0:
                     rho *= 1
-                    theta -= np.pi
+                    #theta -= np.pi
                 closeness_rho = np.isclose(rho, strong_lines[0:n2, 0, 0], atol=50)
                 closeness = np.all([closeness_rho], axis=0)
-                if not any(closeness) and np.allclose(theta,strong_lines[0:n2,0,0],atol=510)==True: #takes away all lines that are in proximity and only keeps other lines with similar theta
+                if not any(closeness) and np.allclose(abs(theta),abs(theta1),rtol=0.000001)==True and intersect(pt1og,pt2og,pt1,pt2)==False: #takes away all lines that are in proximity and only keeps other lines with similar theta
                     strong_lines[n2] = lines[n1]
                     cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
                     cv2.line(img, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
@@ -102,23 +111,21 @@ while True:
     mask = cv2.inRange(imgHSV,lower,upper)
     imgResult = cv2.bitwise_and(img,img,mask=mask)
 
-    mask = 255 - cv2.medianBlur(mask, 3)
+    img = 255 - cv2.medianBlur(img, 3)
 
-    dst = cv2.Canny(mask, 300, 100, None, 3)
+    dst = cv2.Canny(img, 500, 50, None, 3)
     cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
     cdstP = np.copy(cdst)
 
-    lines = cv2.HoughLines(dst, 1, np.pi / 180, 120, None, 500, 0)
-
+    lines = cv2.HoughLines(dst, 1, np.pi / 180, 150, None, 50, 0)
     strong_lines = np.zeros([50, 1, 2])
-
+    print(lines)
     if int(0 if lines is None else 1) == 0:
         cv2.putText(cdst, "No lines detected", (50, 50), 0, 1, (0, 0, 255), 1, 2)
 
     else:
         linedetection(lines,cdst)
-
-    cv2.imshow("Mask Images", mask)
+    #cv2.imshow("Mask Images", mask)
     cv2.imshow("Original Images", img)
     cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
 
